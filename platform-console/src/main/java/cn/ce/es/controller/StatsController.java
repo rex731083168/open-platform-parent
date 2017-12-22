df8f4a59f0f8ff3cb3423d90cd6bfe5bbebcef18
makangwei <makangwei@300.cn>
2017-12-22 13:58:33 +0800

    2017122201

--- platform-console/src/main/java/cn/ce/es/controller/StatsController.java
+++ platform-console/src/main/java/cn/ce/es/controller/StatsController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -30,6 +31,7 @@ public class StatsController {
 	@Autowired
 	@Qualifier("statsService")
 	private IStatsService statsService;
+	
 
 	/**
 	 * 
