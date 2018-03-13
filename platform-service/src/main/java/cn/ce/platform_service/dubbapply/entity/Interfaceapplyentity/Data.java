69e8e63a2167340cdc8ed8b88e0c7a08169bd72d
lenovo <yuyang31471@163.com>
2018-03-13 17:48:42 +0800

    接口测试

---
+++ platform-service/src/main/java/cn/ce/platform_service/dubbapply/entity/Interfaceapplyentity/Data.java
@@ -0,0 +1,286 @@
+/**
+  * Copyright 2018 bejson.com 
+  */
+package cn.ce.platform_service.dubbapply.entity.Interfaceapplyentity;
+
+/**
+ * Auto-generated: 2018-03-13 15:51:1
+ *
+ * @author bejson.com (i@bejson.com)
+ * @website http://www.bejson.com/java2pojo/
+ */
+public class Data {
+
+    private String id;
+    private Integer appId;
+    private String appName;
+    private String appAlias;
+    private String appTypes;
+    private String appUrl;
+    private String appDesc;
+    private String appCatogroyId;
+    private String appTag;
+    private String appStatus;
+    private Integer isRelation;
+    private String resourcetype;
+    private String offresourcetype;
+    private String isSearch;
+    private Integer isShow;
+    private String appCode;
+    private String unitType;
+    private Integer appType;
+    private String owner;
+    private String templateCode;
+    private String productCode;
+    private String domain;
+    private String appPurchaseTime;
+    private String appExpriationTime;
+    private String onlySupportLang;
+    private Integer supportDevice;
+    private String packageName;
+    private Integer isSupportSeo;
+    private Integer isDesignShow;
+    private Integer parentAppId;
+    private String isAppMenu;
+    private String isApp;
+    private String appIcon;
+    private String appCreateTime;
+    public void setId(String id) {
+         this.id = id;
+     }
+     public String getId() {
+         return id;
+     }
+
+    public void setAppId(Integer appId) {
+         this.appId = appId;
+     }
+     public Integer getAppId() {
+         return appId;
+     }
+
+    public void setAppName(String appName) {
+         this.appName = appName;
+     }
+     public String getAppName() {
+         return appName;
+     }
+
+    public void setAppAlias(String appAlias) {
+         this.appAlias = appAlias;
+     }
+     public String getAppAlias() {
+         return appAlias;
+     }
+
+    public void setAppTypes(String appTypes) {
+         this.appTypes = appTypes;
+     }
+     public String getAppTypes() {
+         return appTypes;
+     }
+
+    public void setAppUrl(String appUrl) {
+         this.appUrl = appUrl;
+     }
+     public String getAppUrl() {
+         return appUrl;
+     }
+
+    public void setAppDesc(String appDesc) {
+         this.appDesc = appDesc;
+     }
+     public String getAppDesc() {
+         return appDesc;
+     }
+
+    public void setAppCatogroyId(String appCatogroyId) {
+         this.appCatogroyId = appCatogroyId;
+     }
+     public String getAppCatogroyId() {
+         return appCatogroyId;
+     }
+
+    public void setAppTag(String appTag) {
+         this.appTag = appTag;
+     }
+     public String getAppTag() {
+         return appTag;
+     }
+
+    public void setAppStatus(String appStatus) {
+         this.appStatus = appStatus;
+     }
+     public String getAppStatus() {
+         return appStatus;
+     }
+
+    public void setIsRelation(Integer isRelation) {
+         this.isRelation = isRelation;
+     }
+     public Integer getIsRelation() {
+         return isRelation;
+     }
+
+    public void setResourcetype(String resourcetype) {
+         this.resourcetype = resourcetype;
+     }
+     public String getResourcetype() {
+         return resourcetype;
+     }
+
+    public void setOffresourcetype(String offresourcetype) {
+         this.offresourcetype = offresourcetype;
+     }
+     public String getOffresourcetype() {
+         return offresourcetype;
+     }
+
+    public void setIsSearch(String isSearch) {
+         this.isSearch = isSearch;
+     }
+     public String getIsSearch() {
+         return isSearch;
+     }
+
+    public void setIsShow(Integer isShow) {
+         this.isShow = isShow;
+     }
+     public Integer getIsShow() {
+         return isShow;
+     }
+
+    public void setAppCode(String appCode) {
+         this.appCode = appCode;
+     }
+     public String getAppCode() {
+         return appCode;
+     }
+
+    public void setUnitType(String unitType) {
+         this.unitType = unitType;
+     }
+     public String getUnitType() {
+         return unitType;
+     }
+
+    public void setAppType(Integer appType) {
+         this.appType = appType;
+     }
+     public Integer getAppType() {
+         return appType;
+     }
+
+    public void setOwner(String owner) {
+         this.owner = owner;
+     }
+     public String getOwner() {
+         return owner;
+     }
+
+    public void setTemplateCode(String templateCode) {
+         this.templateCode = templateCode;
+     }
+     public String getTemplateCode() {
+         return templateCode;
+     }
+
+    public void setProductCode(String productCode) {
+         this.productCode = productCode;
+     }
+     public String getProductCode() {
+         return productCode;
+     }
+
+    public void setDomain(String domain) {
+         this.domain = domain;
+     }
+     public String getDomain() {
+         return domain;
+     }
+
+    public void setAppPurchaseTime(String appPurchaseTime) {
+         this.appPurchaseTime = appPurchaseTime;
+     }
+     public String getAppPurchaseTime() {
+         return appPurchaseTime;
+     }
+
+    public void setAppExpriationTime(String appExpriationTime) {
+         this.appExpriationTime = appExpriationTime;
+     }
+     public String getAppExpriationTime() {
+         return appExpriationTime;
+     }
+
+    public void setOnlySupportLang(String onlySupportLang) {
+         this.onlySupportLang = onlySupportLang;
+     }
+     public String getOnlySupportLang() {
+         return onlySupportLang;
+     }
+
+    public void setSupportDevice(Integer supportDevice) {
+         this.supportDevice = supportDevice;
+     }
+     public Integer getSupportDevice() {
+         return supportDevice;
+     }
+
+    public void setPackageName(String packageName) {
+         this.packageName = packageName;
+     }
+     public String getPackageName() {
+         return packageName;
+     }
+
+    public void setIsSupportSeo(Integer isSupportSeo) {
+         this.isSupportSeo = isSupportSeo;
+     }
+     public Integer getIsSupportSeo() {
+         return isSupportSeo;
+     }
+
+    public void setIsDesignShow(Integer isDesignShow) {
+         this.isDesignShow = isDesignShow;
+     }
+     public Integer getIsDesignShow() {
+         return isDesignShow;
+     }
+
+    public void setParentAppId(Integer parentAppId) {
+         this.parentAppId = parentAppId;
+     }
+     public Integer getParentAppId() {
+         return parentAppId;
+     }
+
+    public void setIsAppMenu(String isAppMenu) {
+         this.isAppMenu = isAppMenu;
+     }
+     public String getIsAppMenu() {
+         return isAppMenu;
+     }
+
+    public void setIsApp(String isApp) {
+         this.isApp = isApp;
+     }
+     public String getIsApp() {
+         return isApp;
+     }
+
+    public void setAppIcon(String appIcon) {
+         this.appIcon = appIcon;
+     }
+     public String getAppIcon() {
+         return appIcon;
+     }
+
+    public void setAppCreateTime(String appCreateTime) {
+         this.appCreateTime = appCreateTime;
+     }
+     public String getAppCreateTime() {
+         return appCreateTime;
+     }
+
+}
