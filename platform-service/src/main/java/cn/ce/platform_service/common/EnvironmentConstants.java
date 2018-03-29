f509271641ad4a6234b09ca8ed76f8a20d97b7ee
makangwei <makangwei@300.cn>
2018-03-29 20:03:07 +0800

    2018032901 EnvironmentConstants

---
+++ platform-service/src/main/java/cn/ce/platform_service/common/EnvironmentConstants.java
@@ -0,0 +1,12 @@
+package cn.ce.platform_service.common;
+
+/**
+* @Description : 说明
+* @Author : makangwei
+* @Date : 2018年3月29日
+*/
+public enum EnvironmentConstants {
+
+	local,test,pre,prod;
+}
+
